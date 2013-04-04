40ab551b14929ec4a9b1049553c1783249594879
Stefan Strigler <stefan.strigler@erlang-solutions.com>
2013-04-04 20:40:47 +0200

    fix header

--- apps/ejabberd/src/ejabberd_auth_webhooks.erl
+++ apps/ejabberd/src/ejabberd_auth_webhooks.erl
@@ -1,30 +1,13 @@
-%%%----------------------------------------------------------------------
-%%% File    : ejabberd_auth_cce.erl
-%%% Author  : Stefan Strigler <stefan@strigler.de>
-%%% Purpose : Authentification via CCE data API
-%%% Created : 20 May 2009 by Stefan Strigler <stefan@strigler.de>
+%%% @author Stefan Strigler <stefan.strigler@erlang-solutions.com>
+%%% @copyright (C) 2013, Stefan Strigler
+%%% @doc
 %%%
-%%% Based on ejabberd_auth_internal.erl of ejabberd v2.0.1
-%%%
-%%%
-%%% ejabberd, Copyright (C) 2002-2008   Process-one
-%%%
-%%% This program is free software; you can redistribute it and/or
-%%% modify it under the terms of the GNU General Public License as
-%%% published by the Free Software Foundation; either version 2 of the
-%%% License, or (at your option) any later version.
-%%%
-%%% This program is distributed in the hope that it will be useful,
-%%% but WITHOUT ANY WARRANTY; without even the implied warranty of
-%%% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-%%% General Public License for more details.
-%%%
-%%% You should have received a copy of the GNU General Public License
-%%% along with this program; if not, write to the Free Software
-%%% Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
-%%% 02111-1307 USA
-%%%
-%%%----------------------------------------------------------------------
+%%% @end
+%%% Created :  4 Apr 2013 by Stefan Strigler <stefan.strigler@erlang-solutions.com>
+
+-module(ejabberd_auth_webhooks).
+
+
 
 -module(ejabberd_auth_webhooks).
 -author('stefan@strigler.de').
